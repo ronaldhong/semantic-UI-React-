7272cacde7a220d2312d18df665a513c13fcc956
n221199515 <hong_ronald@hotmail.com>
2018-02-28 18:09:53 -0600

    navbar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,42 @@
 import React, { Component } from 'react';
 import './App.css';
+import {Menu} from 'semantic-ui-react';
+import logo from "./logo.png";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div class="ui three buttons">
-          <button class="ui active button">One</button>
-          <button class="ui button">Two</button>
-          <button class="ui button">Three</button>
-        </div>
-        <div class="ui padded segment">
-          <button class="ui fluid primary button" role="button">Login</button>
-          <div class="ui horizontal divider">Or</div>
-          <button class="ui fluid secondary button" role="button">Sign Up Now</button>
+      {/* //   <Menu>
+      //     <Menu.Menu>
+      //       <Menu.Item>
+      //         <a href="">Menu</a>
+      //       </Menu.Item>
+      //       <Menu.icon>
+      //         <img src={logo} alt="logo" size="large"/>
+      //       </Menu.icon>
+      //     </Menu.Menu>
+      //     <Menu.Menu position="right">
+      //       <Menu.Item>
+      //         <a href="">Login</a>
+      //       </Menu.Item>
+      //       <Menu.Item>
+      //         <a href="">Sign up</a>
+      //       </Menu.Item>
+      //     </Menu.Menu>
+      //   </Menu> */}
+        <div class="ui stackable menu">
+          <div class="item">
+            <img src={logo} />
+          </div>
+          <div class="right menu">
+            <a class="item">Sign-in</a>
+            <a class="item">Home</a>
+            <a class="item">Gallery</a>
+            <a class="item">Contact</a>
+            <a class="item">About US</a>
+            <a class="item">Pricing</a>
+          </div>
         </div>
       </div>
 
