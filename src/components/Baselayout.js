805a299baf4ad349a6e70453ebca7946e9d02c81
n221199515 <hong_ronald@hotmail.com>
2018-03-01 15:17:32 -0600

    added browserHistory for navigation
    note: react-router v3

--- src/components/Baselayout.js
+++ src/components/Baselayout.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import {Menu} from 'semantic-ui-react';
+import {NavLink} from 'react-router-dom';
+import { browserHistory } from 'react-router';
 import logo from "../logo.png";
 
 class Baselayout extends Component {
@@ -11,12 +13,28 @@ class Baselayout extends Component {
             <img class="item" src={logo} alt="logo" height="100px;" width="200px;"/>
           </div>
           <div class="right menu ui segment">
-            <a class="item active">Home</a>
+            {/* <a class="item active">Home</a>
             <a class="item">Gallery</a>
             <a class="item">Contact</a>
             <a class="item">About US</a>
             <a class="item">Pricing</a>
-            <a class="item">Sign-in</a>
+            <a class="item">Sign-in</a> */}
+            <Menu.Item onClick={()=> browserHistory.push('/')}>
+              Home
+            </Menu.Item>
+            <Menu.Item onClick={()=> browserHistory.push('/gallery')}>
+              Gallery
+            </Menu.Item>
+            <Menu.Item onClick={()=> browserHistory.push('/contact')}>
+              Contact
+            </Menu.Item>
+            <Menu.Item onClick={()=> browserHistory.push('/about')}>
+              About
+            </Menu.Item>
+            <Menu.Item onClick={()=> browserHistory.push('/price')}>
+              Pricing
+            </Menu.Item>
+
           </div>
         </Menu>
 
